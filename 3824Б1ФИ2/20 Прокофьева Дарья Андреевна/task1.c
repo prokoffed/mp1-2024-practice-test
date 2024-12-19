4f4fc67b8bc3638ce115f7f5a98852115526fb78
prokoffed <ProkofyevaD2006@yandex.ru>
2024-12-19 10:25:33 +0300

    Update task1.c

--- 3824Б1ФИ2/20 Прокофьева Дарья Андреевна/task1.c
+++ 3824Б1ФИ2/20 Прокофьева Дарья Андреевна/task1.c
@@ -1,7 +1,26 @@
 // Задача 1. Дан непустой массив A из N элементов.
 // Преобразовать его, прибавив к нечетным числам первый элемент,
 // а к четным – последний. Первый и последний элементы массива не менять. 
-void task1(unsigned A[], size_t N)
-{
-  A[0] = 1111111111;
+#include <stdio.h>
+
+
+void task1(unsigned A[], size_t N) {
+   
+    if (N < 2) {
+        printf("The array is too small to complete.\n");
+        return;
+    }
+
+    unsigned first = A[0];
+    unsigned last = A[N - 1];
+
+    for (size_t i = 1; i < N - 1; i++) {
+        if (A[i] % 2 == 0) {
+            A[i] += last; 
+        }
+        else {
+            A[i] += first; 
+        }
+    }
 }
+
