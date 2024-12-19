b83f72cbcaf2699caa0468fb27b938e708713af2
prokoffed <ProkofyevaD2006@yandex.ru>
2024-12-19 10:20:48 +0300

    Update task2.c

--- 3824Б1ФИ2/20 Прокофьева Дарья Андреевна/task2.c
+++ 3824Б1ФИ2/20 Прокофьева Дарья Андреевна/task2.c
@@ -2,7 +2,23 @@
 // Назовем «серией» группу подряд идущих одинаковых элементов, 
 // а длиной серии – количество этих элементов (длина серии может быть равна 2).
 // Найти число серий длины 3 в массиве.
-int task2(unsigned A[], size_t n)
-{
-  return -1;
+#include <stdio.h>
+
+int task2(unsigned A[], size_t n) {
+    if (n < 3) {
+        return 0; 
+    }
+
+    int count = 0;
+
+    
+    for (size_t i = 0; i < n - 2; i++) {
+        if (A[i] == A[i + 1] && A[i] == A[i + 2]) {
+            count++; 
+        }
+    }
+
+    return count;
 }
+
+
